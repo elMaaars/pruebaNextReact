6ca86b39cdefca98ccb965b1a63b5a6f8c473ec0
Bruno Arce R <brunoarce0@gmail.com>
2024-03-15 15:22:24 -0300

    Added other recipees

---
+++ prueba/src/pages/calzonesRotos.tsx
@@ -0,0 +1,36 @@
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import Box from '@mui/material/Box';
+
+
+export default function Cazuela() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <h1> Calzones rotos </h1>
+        <h2> AKA Broken underwear </h2>
+      </div>
+    
+      <div className={styles.recipeeCard}>
+      <div>
+        <h1>
+          Ingredientes:
+        </h1>
+        <ul>
+          <li>Agua</li>
+          <li>Harina</li>
+          <li>Levadura</li>
+          <li>Aceite</li>
+          <li>Azúcar flor</li>
+        </ul>
+      </div>
+        <Image
+          src="/calzonesRotos.jpeg"
+          width={600}
+          height={600}
+          alt="Calzones Rotos"
+        />
+      </div>
+    </div>
+  );
+}
---
+++ prueba/src/pages/sopaipillas.tsx
@@ -0,0 +1,36 @@
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import Box from '@mui/material/Box';
+
+
+export default function Cazuela() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <h1> Sopaipilla </h1>
+        <h2> Ricas sopaipillas con ketchup </h2>
+      </div>
+    
+      <div className={styles.recipeeCard}>
+      <div>
+        <h1>
+          Ingredientes:
+        </h1>
+        <ul>
+          <li>Harina</li>
+          <li>Manteca</li>
+          <li>Sal</li>
+          <li>Aceite</li>
+          <li>Levadura</li>
+        </ul>
+      </div>
+        <Image
+          src="/sopaipillas.jpeg"
+          width={600}
+          height={600}
+          alt="Sopaipillas"
+        />
+      </div>
+    </div>
+  );
+}
